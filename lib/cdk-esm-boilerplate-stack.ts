b9a6a325b6670ded0535193cee0e1a240fe05ea1
esplo <esplo@users.noreply.github.com>
2024-04-26 19:36:50 +0900

    add banner for esm

--- lib/cdk-esm-boilerplate-stack.ts
+++ lib/cdk-esm-boilerplate-stack.ts
@@ -16,6 +16,8 @@ export class CdkEsmBoilerplateStack extends cdk.Stack {
       bundling: {
         format: cdk.aws_lambda_nodejs.OutputFormat.ESM,
         mainFields: ["module", "main"],
+        banner:
+          "const require = (await import('node:module')).createRequire(import.meta.url);const __filename = (await import('node:url')).fileURLToPath(import.meta.url);const __dirname = (await import('node:path')).dirname(__filename);",
       },
     });
   }
